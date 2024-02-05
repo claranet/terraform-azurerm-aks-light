d61bf2fe132b563a03c24e5a8c03a39a25e4e9c6
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2024-02-05 18:20:38 +0100

    AZ-1348: Add Data Collection Rule

--- variables-logs.tf
+++ variables-logs.tf
@@ -26,3 +26,41 @@ variable "custom_diagnostic_settings_name" {
   type        = string
   default     = "default"
 }
+
+variable "logs_kube_audit_enabled" {
+  description = "Whether to include `kube-audit` and `kube-audit-admin` logs from diagnostics settings collection. Enabling this can increase your Azure billing."
+  type        = bool
+  default     = false
+}
+
+variable "data_collection_rule" {
+  description = "AKS Data Collection Rule configuration."
+  type = object({
+    log_analytics_workspace_id = optional(string)
+    data_streams = optional(list(string), [
+      "Microsoft-ContainerLog",
+      "Microsoft-ContainerLogV2",
+      "Microsoft-KubeEvents",
+      "Microsoft-KubePodInventory",
+      "Microsoft-InsightsMetrics",
+      "Microsoft-ContainerInventory",
+      "Microsoft-ContainerNodeInventory",
+      "Microsoft-KubeNodeInventory",
+      "Microsoft-KubeServices",
+      "Microsoft-KubePVInventory"
+    ])
+    namespaces_filter = optional(list(string), [
+      "kube-system",
+      "gatekeeper-system",
+      "kube-node-lease",
+      "calico-system",
+    ])
+    namespace_filtering_mode = optional(string, "Exclude")
+    data_collection_interval = optional(string, "5m")
+    container_log_v2_enabled = optional(bool, true)
+
+  })
+  default  = {}
+  nullable = false
+}
+>>>>>>> e518624 (AZ-1348: Add Data Collection Rule)
